2f2a3a2c5af8fed7e681838d8b2d179400eca42d
mrkingsleyho <94074831+mrkingsleyho@users.noreply.github.com>
2022-03-01 21:26:25 -0500

    initial commit

---
+++ src/data.js
@@ -0,0 +1,33 @@
+const questions = [
+  {
+    id: 1,
+    title: 'What is the Interac e-Transfer limit?',
+    info:
+      'Here are the daily, weekly and monthly limits for sending and receiving Interac e-Transfers: The daily limit is $3,000, the weekly limit is $10,000 and the monthly limit is $30,000. ',
+  },
+  {
+    id: 2,
+    title: 'How do I send money by Interac e-transfer®?',
+    info:
+      'You can send or receive money by Interac e-Transfer® using your Bank of Toronto Chequing Account. There is a fixed fee to receive or send money by Interac e-transfer.',
+  },
+  {
+    id: 3,
+    title: 'My Client Card and/or Credit Card is lost or stolen. What should I do?',
+    info:
+      'If your Bank of Toronto Client Card is lost or stolen, please report it right away. Or call us at 1-888-416-6666 to report your Card as lost or stolen. A replacement Card will arrive within 5 to 7 days, free of charge.',
+  },
+  {
+    id: 4,
+    title: 'How do I invest with Bank of Toronto?',
+    info:
+      'Super easy! Just log in to the Client Portal website and navigate towards the Client features page. For a limited time only, you will receive a surprise stock if you sign up with us today!',
+  },
+  {
+    id: 5,
+    title: 'What is the rate for Bank of Toronto Savings Account?',
+    info:
+      'As of January 1, 2022, the rate of our Savings Account starts at 5%. If you have a Chequing and Investing account with us, the Savings Account rate will start at 7%. ',
+  },
+]
+export default questions
